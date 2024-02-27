df7ce40187feac1426ad164448414ad2be742227
Jagadeesh <jagadeesh@codifi.in>
2024-02-27 10:11:03 +0530

    Initial Commit

---
+++ src/test/java/jaga/codifi/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package jaga.codifi;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
